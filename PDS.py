12a38c63822f51aba13063ae5bd39b883cd44f37
nickert <nick_hyves@Hotmail.com>
2014-05-15 14:24:37 +0200

    DGA toegevoegt
    DGA functie toegevoegt, tabs in spaties veranderd

--- PDS.py
+++ PDS.py
@@ -2,12 +2,15 @@
 # PassiveDNSSearcher [PDS]
 # version 1.0
 # devoleped in python v3.4
-# born Thursday 27 March 2014 A.D.				  
-# made by Olaf Elzinga & Nick de Bruijn Van Melis En Mariekerke	 
+#requires tldextract module
+# born Thursday 27 March 2014 A.D.                  
+# made by Olaf Elzinga & Nick de Bruijn Van Melis En Mariekerke     
 # import
 import getopt, sys, collections
 import tldextract
 import time
+from pprint import pprint
+import re
 
 # global vars
 log = None
@@ -17,11 +20,12 @@ def main():
     global log
     logCheck = False
     try:
-        options, remainder = getopt.getopt(sys.argv[1:], 't:l:hs' # before : is for value with argu / after : no arg[litle bit strange]
-															, ['ttl=', 
-		                                                       'log=',
-		                                                       'help',
-		                                                        'statistic',])
+        options, remainder = getopt.getopt(sys.argv[1:], 't:l:hsd' # before : is for value with argu / after : no arg[litle bit strange]
+                                                            , ['ttl=', 
+                                                               'log=',
+                                                               'help',
+                                                                'statistic',
+                                                                'dga',])
     except getopt.GetoptError as err:
         print(err)
         print("type -h for more information!")
@@ -37,7 +41,7 @@ def main():
                 help()
                 sys.exit()
         if logCheck == True:
-            for opt, arg in options:   	
+            for opt, arg in options:       
                 if opt in ('-t', '--ttl'): # time to live !work in progrese!
                     TTL(arg)
                 elif opt in ('-s', '--statistic'):
@@ -45,73 +49,91 @@ def main():
                 elif opt in ('-a', '--all'):
                     statistic()
                     TTL(arg)
+                elif opt in ('-d', '--dga'):
+                    DGA()                 
         else:
             print("Select a log with -l \nFor more info check --help")
             sys.exit()
 
     else:
-    	print("Give an option to run this script \nType -h for more information!")
-    	sys.exit()
+        print("Give an option to run this script \nType -h for more information!")
+        sys.exit()
 
 def help():
-	print("-s 	:Show all info about a log file")
-	print("-t   :Print all lines with less than Time-to-Live is specified")
+    print("-s     :Show all info about a log file")
+    print("-t   :Print all lines with less than Time-to-Live is specified")
 
 def statistic():
-	tldList={}
-	DNSsList = {}
-	DNScList = {}
-	QTList={}
-	for line in log:
-		array = (line.split('||')) #split each line get from passiveDNS logf ile
-		print (array)
-		#Top Level Domain [TLD]
-		tld = tldextract.extract(array[4])
-		tld = tld.suffix
-		#tld = 'nl'
-		if not tld:
-			tld = "not existing"
-		fillList(tldList, tld)
-		#DNS servers involved 
-		DNSs = (array[2])
-		DNSc = (array[1])
-		fillList(DNScList, DNSc)
-		fillList(DNSsList, DNSs)
-		#Query Type [Record (e.g. A, CNAME, MX)]
-		QT = (array[5])
-		fillList(QTList, QT)
+    tldList={}
+    DNSsList = {}
+    DNScList = {}
+    QTList={}
+    for line in log:
+        array = (line.split('||')) #split each line get from passiveDNS log file
+        print (array)
+        #Top Level Domain [TLD]
+        tld = tldextract.extract(array[4])
+        tld = tld.suffix
+        #tld = 'nl'
+        if not tld:
+            tld = "not existing"
+        fillList(tldList, tld)
+        #DNS servers involved 
+        DNSs = (array[2])
+        DNSc = (array[1])
+        fillList(DNScList, DNSc)
+        fillList(DNSsList, DNSs)
+        #Query Type [Record (e.g. A, CNAME, MX)]
+        QT = (array[5])
+        fillList(QTList, QT)
 
-	print("\nDNSserver")
-	printList(DNSsList)
-	print("\nDNSclients")
-	printList(DNScList)
-	print("\nTop Level Domains")
-	printList(tldList)
-	print("\nQuery types")
-	printList(QTList)
+    print("\nDNSserver")
+    printList(DNSsList)
+    print("\nDNSclients")
+    printList(DNScList)
+    print("\nTop Level Domains")
+    printList(tldList)
+    print("\nQuery types")
+    printList(QTList)
 
 def TTL(arg):
-	TTL=[]
-	for line in log:
-		array = (line.split('||'))
-		TTLv = array[7]
-		if int(TTLv)<int(arg):
-			TTL.append(line)
-	print (''.join(TTL))
+    TTL=[]
+    for line in log:
+        array = (line.split('||'))
+        TTLv = array[7]
+        if int(TTLv)<int(arg):
+            TTL.append(line)
+    print (''.join(TTL))
+
+def DGA():
+    DGA={}
+
+    patterns = [re.compile('[a-z0-9]{40,43}\.[a-z]{2,3}\.'), re.compile('[a-z]{15}\.info\.'), re.compile('[a-z]{15,30}\.biz\.')]
+
+    for line in log:
+        array = line.split('||')
+
+        for pattern in patterns:
+            if pattern.match(array[4]):
+                count = DGA.get(query, 0)
+                DGA[query] = count + 1
+                break
+
+    pprint(sorted(DGA.items(), key=lambda item: item[1]))
 
 def printList(item):
-	for key, value in item.items():
-		spaceLength = 30-len(key)
-		line = key,''.ljust(spaceLength),":", value
+    for key, value in item.items():
+        spaceLength = 30-len(key)
+        line = key,''.ljust(spaceLength),":", value
 
-		print(key,''.ljust(spaceLength),":", value)	
-		filex.write(str(line)+"\n")
+        print(key,''.ljust(spaceLength),":", value)    
+        filex.write(str(line)+"\n")
 
 def fillList(x, y):
-	if y not in x:
-		x[y] = 1
-	else:
-		x[y] += 1
+    if y not in x:
+        x[y] = 1
+    else:
+        x[y] += 1
 
 if __name__ == "__main__":
     main()
